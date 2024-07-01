484d3e0feb7d88a0ceaf75cebdd764f9d5183873
Sascha Frevel <3075075+sfrevel@users.noreply.github.com>
2024-07-01 09:22:49 +0200

    add testing stage

---
+++ test/deployment_test.go
@@ -0,0 +1,22 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAKS(t *testing.T) {
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir:    "../env/test-config",
+		TerraformBinary: "/usr/local/bin/terragrunt",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.Apply(t, terraformOptions)
+
+	output := terraform.Output(t, terraformOptions, "resource_name")
+	assert.Equal(t, "rg_terratest_project_name", output)
+}
